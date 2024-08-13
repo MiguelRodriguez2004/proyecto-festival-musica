f7b9bc9286085718033b1d30ff9bf18f63f25156
Miguel Rodriguez <miguelangelnunezrodriguez2004@gmail.com>
2024-08-13 12:49:04 -0500

    Finalizacion del proyecto, detalles en el front y validaciones en JS.

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
+    navegacionFija();
     crearGaleria();
+    resaltarEnlace();
+    scrollNav();
 })
+
+function navegacionFija() {
+    const header = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+
+    document.addEventListener('scroll', function() {
+        if (sobreFestival.getBoundingClientRect().bottom < 1) {
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+    })
+}
 
 function crearGaleria() {
     const cantidad_imagenes = 16
@@ -28,9 +44,16 @@ function mostrarImagen(i) {
     // Crear Modal
     const modal = document.createElement('div');
     modal.classList.add('modal');
-    modal.onclick = cerrarModal
+    modal.onclick = cerrarModal;
+
+    // Boton cerrar modal
+    const btnCerrarModal = document.createElement('button');
+    btnCerrarModal.textContent = 'X';
+    btnCerrarModal.classList.add('btn-cerrar');
+    btnCerrarModal.onclick = cerrarModal;
 
     modal.appendChild(imagen);
+    modal.appendChild(btnCerrarModal);
 
     const body = document.querySelector('body');
     body.classList.add('overflow-hidden');
@@ -47,4 +70,41 @@ function cerrarModal() {
         const body = document.querySelector('body');
         body.classList.remove('overflow-hidden');
     }, 500);
+}
+
+function resaltarEnlace() {
+    document.addEventListener('scroll', function() {
+        const sections = document.querySelectorAll('section')
+        const navLinks = document.querySelectorAll('.navegacion-principal a')
+
+        let actual = '';
+        sections.forEach( section => {
+            const sectionTop = section.offsetTop
+            const sectionHeight = section.clientHeight
+            if(window.scrollY >= (sectionTop - sectionHeight / 3 ) ) {
+                actual = section.id
+            }
+        })
+
+        navLinks.forEach(link => {
+            link.classList.remove('active')
+            if(link.getAttribute('href') === '#' + actual) {
+                link.classList.add('active')
+            }
+        })
+    })
+}
+
+function scrollNav() {
+    const navLinks = document.querySelectorAll('.navegacion-principal a')
+
+    navLinks.forEach( link => {
+        link.addEventListener('click', e => {
+            e.preventDefault()
+            const sectionScroll = e.target.getAttribute('href')
+            const section = document.querySelector(sectionScroll)
+
+            section.scrollIntoView({behavior: 'smooth'})
+        })
+    })
 }
